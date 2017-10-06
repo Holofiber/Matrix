2b4fa1bc94bbc21fc5b65da6e0bf8548a5580cc3
Holofiber <aminus2010@gmail.com>
2017-10-06 22:38:05 +0300

    Matrix n*m with ramka

--- Program.cs
+++ Program.cs
@@ -10,35 +10,89 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int k = 3, l = 3;
-            //  Console.WriteLine("Enter your first parametr: ");
-            //  k = Convert.ToInt32(Console.ReadLine());
-            //  Console.WriteLine("Enter your second parametr: ");
-            //   l = Convert.ToInt32(Console.ReadLine());
-            //  Console.WriteLine("Your Matrix {0}*{1} : ", k, l);
+            int k = 3, l = 3, m=0;
+            Console.WriteLine("Enter your first parametr: ");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second parametr: ");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Matrix {0}*{1} : ", k, l);
             int[,] myMatrix;
             myMatrix = new int[k, l];
-
+            int z = l;
             for (int i = 0; i < k; i++)
 
-                for (int j = 0; j < l; j++)
-                    myMatrix[i, j] = i * j;
+            for (int j = 0; j < l; j++)
+                myMatrix[i, j] = i * j;
 
             for (int i = 0; i < k; i++)
             {
-                for (int j = 0; j < l; j++)
+                for (int j = 0; j < l; j++) ;
 
-                    Console.Write(myMatrix[i, j] + " \t");
+
+                // Console.Write($" {myMatrix[i, j],4}" + "\t");
+                // Console.WriteLine();
+            }
+
+            //Вверх матриці, будується один раз
+            Console.Write("╔═══");
+            for (int n = 0; n < (k - 1); n++)
+                Console.Write("╦═══");
+            Console.Write("╗");
+            Console.WriteLine();
+
+
+            //Вертикальні рядки матриці
+            for (m = 0; m < (l-1); m++)
+            {
+                Console.Write("║");
+                for (int i = 0; i < k; i++)
+                {
+                    for (int j = 0; j < l; j++)
+                        myMatrix[i, j] = i * j;
+                    Console.Write($"{myMatrix[i, m],3}" + "║");
+                }
+
+                //
+                Console.WriteLine();
+                Console.Write("╠══");
+                for (int n = 0; n < (k - 1); n++)
+                    Console.Write("═╬══");
+                Console.Write("═╣");
                 Console.WriteLine();
             }
 
-            Console.WriteLine("\t╔═════╦═════╦═════╗");
-            Console.WriteLine($"\t║ {myMatrix[0, 0],3} ║ {myMatrix[0, 1],3} ║ { myMatrix[0, 2],3} ║");
-            Console.WriteLine("\t╠═════╬═════╬═════╣");
-            Console.WriteLine($"\t║ {myMatrix[1, 0],3} ║ {myMatrix[1, 1],3} ║ { myMatrix[1, 2],3} ║");
-            Console.WriteLine("\t╠═════╬═════╬═════╣");
-            Console.WriteLine($"\t║ {myMatrix[2, 0],3} ║ {myMatrix[2, 1],3} ║ { myMatrix[2, 2],3} ║");
-            Console.WriteLine("\t╚═════╩═════╩═════╝");
+            // Останній рядок
+            
+            Console.Write("║");
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < l; j++)
+                    myMatrix[i, j] = i * j;
+                Console.Write($"{myMatrix[i,(l-1)],3}" + "║");
+            }
+            Console.WriteLine();
+
+            //Низ матриці будується один раз
+            Console.Write("╚═══");
+            for (int n = 0; n < (k - 1); n++)
+                Console.Write("╩═══");
+            Console.Write("╝");
+            Console.WriteLine();
+
+
+
+
+
+
+
+            //Console.WriteLine();
+            //Console.WriteLine("\t╔═════╦═════╦═════╗");
+            //Console.WriteLine($"\t║ {myMatrix[0, 0],3} ║ {myMatrix[0, 1],3} ║ { myMatrix[0, 2],3} ║");
+            //Console.WriteLine("\t╠═════╬═════╬═════╣");
+            //Console.WriteLine($"\t║ {myMatrix[1, 0],3} ║ {myMatrix[1, 1],3} ║ { myMatrix[1, 2],3} ║");
+            //Console.WriteLine("\t╠═════╬═════╬═════╣");
+            //Console.WriteLine($"\t║ {myMatrix[2, 0],3} ║ {myMatrix[2, 1],3} ║ { myMatrix[2, 2],3} ║");
+            //Console.WriteLine("\t╚═════╩═════╩═════╝");
 
 
             Console.WriteLine();
